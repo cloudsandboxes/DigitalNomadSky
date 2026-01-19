8baa584794b7c598f6d8908bccdf48e191fd57f0
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-19 15:55:37 +0100

    Update app.py

--- code/nomadsky-engine/UI/app.py
+++ code/nomadsky-engine/UI/app.py
@@ -1,4 +1,5 @@
 #py -m pip install pywebview
 #py -m pip install flask flask-cors
 #py -m pip install azure-identity azure-mgmt-compute
+#pi -m pip install azure-mgmt-resource
 #py -m pip install requests
