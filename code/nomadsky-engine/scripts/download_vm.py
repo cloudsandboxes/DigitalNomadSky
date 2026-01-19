4b526654275015a4e7b91c80b2d3609bdc844dbf
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-19 22:00:07 +0100

    Update download_vm.py

--- code/nomadsky-engine/scripts/download_vm.py
+++ code/nomadsky-engine/scripts/download_vm.py
@@ -1,17 +1,40 @@
 import sys
+from azure.identity import InteractiveBrowserCredential
+from azure.mgmt.compute import ComputeManagementClient
+import sys
+import json
 
 # Get arguments
 source = sys.argv[1]
 destination = sys.argv[2]
 vmname = sys.argv[3]
 
+# Extract specific value from previous files
+shared_data_json = sys.argv[4]  # 4th argument
+shared_data = json.loads(shared_data_json)
+vm_resource_id = shared_data.get('resource_id', '')
+
+try:
+    subscription_id = vm_resource_id
+    resource_group = parts[3]
+except IndexError:
+        raise Exception(f" Invalid resource ID format: '{vm_resource_id}' ")
+        return
+# Authenticate interactively
+tenant_id = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
+#credential = InteractiveBrowserCredential(tenant_id=tenant_id)
 
 
-    result = {
-      'message': f"VM '{vmname}' has been deallocated successfully!",
-      'resource_id': vm_resource_id
+
+#add logic
+
+
+
+result = {
+   'message': f"VM '{vmname}' has been downloaded successfully!",
+   'resource_id': vm_resource_id
     }
-    print(json.dumps(result))
+print(json.dumps(result))
 
 
 
