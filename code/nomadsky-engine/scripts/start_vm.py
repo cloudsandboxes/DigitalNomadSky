74172339d60017ca5469bf171d4047598e9a49d3
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-22 11:07:40 +0100

    Update start_vm.py

--- code/nomadsky-engine/scripts/start_vm.py
+++ code/nomadsky-engine/scripts/start_vm.py
@@ -1,40 +1,35 @@
 import sys
-from azure.identity import InteractiveBrowserCredential
-from azure.mgmt.compute import ComputeManagementClient
-import sys
 import json
 
 # Get arguments
 source = sys.argv[1]
 destination = sys.argv[2]
-vmname = sys.argv[3]
+vmname = sys.argv[3].lower()
+shareddata_json = sys.argv[4]
+shared_data = json.loads(shareddata_json)
 
-# Extract specific value from previous files
-shared_data_json = sys.argv[4]  # 4th argument
-shared_data = json.loads(shared_data_json)
-vm_resource_id = shared_data.get('resource_id', '')
-
-try:
-    subscription_id = vm_resource_id
-    
-except IndexError:
-        raise Exception(f" Invalid resource ID format: '{vm_resource_id}' ")
-        
-# Authenticate interactively
-tenant_id = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
-#credential = InteractiveBrowserCredential(tenant_id=tenant_id)
-
-
-
-#add logic
-
-
-
-result = {
-   'message': f"VM '{vmname}' has been started in '{destination}'!",
-   'resource_id': vm_resource_id
-    }
-print(json.dumps(result))
+if destination == 'azure':
+      # Azure SDK code to find VM
+      sys.path.append(r"C:/projects/nomadsky/code/Microsoft")
+      import config
+      from start_vm import start_vm
+          
+      try:
+            nic = start_vm(shared_data)
+            result = {
+             'message': f"the VM '{vmname}' has started succesfully in '{destination}'!",
+             }
+            print(json.dumps(result))
+      except IndexError:
+        raise Exception(f" something went wrong the vm is not created.")
+   
+elif destination == 'aws':
+   a='empty'
+   #     # AWS boto3 code to find VM
+   # etc.
 
 
-
+#from helpers import my_function
+#result = my_function(5)
+#exportdisktype = shared_data.get('exportdisktype', '')
+#a, b = my_function(10)
