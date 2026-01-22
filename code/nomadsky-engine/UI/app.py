5385b265e45b3406db9afb8503732b6aa61d8730
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-22 10:48:48 +0100

    Update app.py

--- code/nomadsky-engine/UI/app.py
+++ code/nomadsky-engine/UI/app.py
@@ -5,6 +5,6 @@ py -m pip install azure-mgmt-resource
 py -m pip install requests
 py -m pip install azure-storage-blob
 py -m pip install azure-mgmt-storage
-py -m pip install azure-core-exceptions
+py -m pip install  azure-core
 py -m pip install  azure-mgmt-network
 py -m pip install azure-mgmt-compute
