1033c15f40883c07318144da87d459d0d4eec80c
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-22 14:17:17 +0100

    Update fetch_vm.py

--- code/nomadsky-engine/scripts/fetch_vm.py
+++ code/nomadsky-engine/scripts/fetch_vm.py
@@ -1,4 +1,41 @@
 import sys
+import json
+
+# Get arguments
+source = sys.argv[1]
+destination = sys.argv[2]
+vmname = sys.argv[3].lower()
+
+if destination == 'azure':
+      # Azure SDK code to find VM
+      sys.path.append(r"C:/projects/nomadsky/code/Microsoft")
+      import config
+      from fetching_vm import fetch_vm
+          
+      try:
+            vmname,f,df,d,f,f,f,d = fetch_vm(vmname)
+            result = {
+             'message': f"the VM '{vmname}' has started succesfully in '{destination}'!",
+             'vmname'; f      
+             }
+            print(json.dumps(result))
+      except IndexError:
+        raise Exception(f" something went wrong the vm is not created.")
+   
+elif destination == 'aws':
+   a='empty'
+   #     # AWS boto3 code to find VM
+   # etc.
+
+
+#from helpers import my_function
+#result = my_function(5)
+#exportdisktype = shared_data.get('exportdisktype', '')
+#a, b = my_function(10)
+
+
+
+import sys
 import subprocess
 # fetching VM
 
