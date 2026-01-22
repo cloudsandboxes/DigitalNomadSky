a45e4bc9d179fa58c8338d1389b88504d1e2f436
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-22 11:04:37 +0100

    Update create_network.py

--- code/nomadsky-engine/scripts/create_network.py
+++ code/nomadsky-engine/scripts/create_network.py
@@ -1,41 +1,39 @@
 import sys
-from azure.identity import InteractiveBrowserCredential
-from azure.mgmt.compute import ComputeManagementClient
-import sys
 import json
 
 # Get arguments
 source = sys.argv[1]
 destination = sys.argv[2]
-vmname = sys.argv[3]
+vmname = sys.argv[3].lower()
+shareddata_json = sys.argv[4]
+shared_data = json.loads(shareddata_json)
 
-# Extract specific value from previous files
-shared_data_json = sys.argv[4]  # 4th argument
-shared_data = json.loads(shared_data_json)
-vm_resource_id = shared_data.get('resource_id', '')
+if destination == 'azure':
+      # Azure SDK code to find VM
+      sys.path.append(r"C:/projects/nomadsky/code/Microsoft")
+      import config
+      from create_network import create_network
+          
+      try:
+            nic = create_network(shared_data)
+            result = {
+             'message': f"the vnet and nic are succesfully created in '{destination}'!",
+             'nic_id' : nic
+             }
+            print(json.dumps(result))
+      except IndexError:
+        raise Exception(f" the vnet and/nic are not created.")
+   
+elif destination == 'aws':
+   a='empty'
+   #     # AWS boto3 code to find VM
+   # etc.
 
-try:
-    subscription_id = vm_resource_id
-    
-except IndexError:
-        raise Exception(f" Invalid resource ID format: '{vm_resource_id}' ")
-        
-# Authenticate interactively
-tenant_id = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
-#credential = InteractiveBrowserCredential(tenant_id=tenant_id)
 
-
-
-#add logic
-
-
-
-result = {
-   'message': f"VM '{vmname}' has now a new network!",
-   'resource_id': vm_resource_id
-    }
-print(json.dumps(result))
-
+#from helpers import my_function
+#result = my_function(5)
+#exportdisktype = shared_data.get('exportdisktype', '')
+#a, b = my_function(10)
 
 
 
