473903cf64b13146b89e4b4f86c94aec53032c10
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-29 13:53:35 +0100

    Create git-clone.ps1

---
+++ code/first-installation/git-clone.ps1
@@ -0,0 +1,5 @@
+$GithubRepoUrl = "https://github.com/cloudsandboxes/nomadsky.git"
+$CloneDirectory = "C:\Projects"
+$repoName = ($GithubRepoUrl -split '/')[-1] -replace '\.git$', ''
+$repoPath = Join-Path $CloneDirectory $repoName
+git clone $GithubRepoUrl $repoPath
