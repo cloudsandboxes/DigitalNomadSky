6bd970444cec89013b9fcb03e347ed54fb4d394e
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-01-31 00:22:29 +0100

    Update transform_vm.py

--- code/nomadsky-engine/scripts/transform_vm.py
+++ code/nomadsky-engine/scripts/transform_vm.py
@@ -42,7 +42,7 @@ else:
             if destination == "azure":
                     ossize= os.path.getsize(output_path)
                     newsize = math.ceil(ossize/ (1024 * 1024))
-                    subprocess.run([qemu_path, "resize", output_path, +1M], check=True)
+                    subprocess.run([qemu_path, "resize", output_path, "+1M"], check=True)
                     #f"{newsize}M"
         
             #subprocess.run([qemu_path, "convert", "-O", importdisktype, output_path, output_disk_path], check=True)
