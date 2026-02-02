9a632dc831073a9f96c37d4155b7012bb716d9ec
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 11:49:30 +0100

    Update config.py

--- code/Cyso/config.py
+++ code/Cyso/config.py
@@ -1,3 +1,9 @@
 # configuation parameters for Cyso.cloud or referenced by the old name: https://my.fuga.cloud/account/api-endpoints
 
+# Your application credential is to login to your cloud environment 
 OS_APPLICATION_CREDENTIAL_ID = '33730d2e61274dd584f0d7b2fa846fba'
+
+# destination parameters:
+nics = [{"net-id": "496c99b9-4ae0-4cde-b648-d7412832b81b"}]
+network_name="public"
+    
