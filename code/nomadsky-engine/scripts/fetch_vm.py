0b25bc4f4220563aa592b91d8c1357b71bf0dc98
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 16:04:38 +0100

    Update fetch_vm.py

--- code/nomadsky-engine/scripts/fetch_vm.py
+++ code/nomadsky-engine/scripts/fetch_vm.py
@@ -71,7 +71,7 @@ else:
 #Find optimal disktypeformat
 # ------
 
-#sys.path.append(r"C:/projects/nomadsky/code/Vendor_Agnostic")
+sys.path.append(r"C:/projects/nomadsky/code/nomadsky-engine/scripts")
 from general_parameters import preferred_type
 
 def find_best_format(source_platform, destination_platform):
