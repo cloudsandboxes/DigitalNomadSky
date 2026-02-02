9f6bcfe73e6fc9e5b8480b046dbe03b52e50e788
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 11:51:23 +0100

    Update config.py

--- code/Cyso/config.py
+++ code/Cyso/config.py
@@ -4,6 +4,5 @@
 OS_APPLICATION_CREDENTIAL_ID = '33730d2e61274dd584f0d7b2fa846fba'
 
 # destination parameters:
-nics = [{"net-id": "496c99b9-4ae0-4cde-b648-d7412832b81b"}]
-network_name="public"
-    
+nics = [{"net-id": "496c99b9-4ae0-4cde-b648-d7412832b81b"}]  #network id
+
