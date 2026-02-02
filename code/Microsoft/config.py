d5b1a3d8779fdfe6478f22162f957ad698957468
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-02 11:46:50 +0100

    Update config.py

--- code/Microsoft/config.py
+++ code/Microsoft/config.py
@@ -1,11 +1,11 @@
-tenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
-exportdisktype = "vhd"
-importdisktype = ("vhd")
+#Parameters to find your VM. 
+tenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"  # your tenant-id, this is used to find VMs.
 
-# import osdisk variables
-storage_account_name = "compliceert20"
-location = "westeurope"
-container_name = "vhds"
-subscription_id = "41aff5e1-41c9-4509-9fcb-d761d7f33740"
-resource_group = "output"
-destionationtenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6"
+#Parameters to upload your VM.
+location = "westeurope"  # The VM will be created in this location.
+destionationtenantid = "78ba35ee-470e-4a16-ba92-ad53510ad7f6" # the VM will be creaed in this tenant
+subscription_id = "41aff5e1-41c9-4509-9fcb-d761d7f33740" # the VM will be created in this subscription
+resource_group = "output" # the VM will be created in this resource group. 
+
+storage_account_name = "compliceert20"   # a temp storage account to upload the disk file. 
+container_name = "vhds"  # the temp container name to upload the disk file.  
