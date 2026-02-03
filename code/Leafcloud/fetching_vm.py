f8b97f9c1c827275bedb667afd993c5fc8959b21
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-02-03 12:21:00 +0100

    Update fetching_vm.py

--- code/Leafcloud/fetching_vm.py
+++ code/Leafcloud/fetching_vm.py
@@ -17,7 +17,7 @@ def fetch_vm (vmname):
    from keystoneauth1.identity import v3
    import getpass
    import json
-   sys.path.append(r"C:/projects/nomadsky/code/Leafcloud")
+   sys.path.append(r"C:/projects/digitalnomadsky/code/Leafcloud")
    import tkinter as tk
    from tkinter import simpledialog
 
