6af8ef3a4f170846c631fc3e8369c294dee52f13
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-04-24 16:51:33 +0200

    Create config.py

---
+++ code/Stackit/config.py
@@ -0,0 +1,12 @@
+# configuation parameters for Leaf.cloud
+
+# Your application credential is to login to your cloud environment 
+OS_APPLICATION_CREDENTIAL_ID = '6e064903743147188cf917074e71d06c'
+sourcecloudurl = "https://https://iaas.api.eu01.stackit.cloud:5000"  # location of the current cloud environment
+
+
+
+
+# Destination parameters:
+nics = [{"net-id": "ee54f79e-d33a-4866-8df0-4a4576d70243"}]  #network id
+destinationcloudurl = "https://https://iaas.api.eu01.stackit.cloud:5000"  
