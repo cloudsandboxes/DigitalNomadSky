64726adb49bb95578938444bdd48559e13cfdb2e
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2026-06-25 17:41:58 +0200

    Update config.py

--- code/T-systems/config.py
+++ code/T-systems/config.py
@@ -1,1 +1,12 @@
-a
+# configuation parameters for T-systems cloud
+
+# Your application credential is to login to your cloud environment 
+OS_APPLICATION_CREDENTIAL_ID = '6e064903743147188cf917074e71d06c'
+sourcelocation = 'eu-nl' # to complete the url
+destinationlocation = 'eu-nl' # to complete the url
+sourcecloudurl = f"https://iam.{sourcelocation}.otc.t-systems.com:443/v3"  # location of the current cloud environment
+
+
+# Destination parameters:
+nics = [{"net-id": "ee54f79e-d33a-4866-8df0-4a4576d70243"}]  #network id
+destinationcloudurl = sourcecloudurl = f"https://iam.{destinationlocation}.otc.t-systems.com:443/v3"  
